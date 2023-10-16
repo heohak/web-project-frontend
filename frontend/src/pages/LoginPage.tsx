f112c7610c47a236b90b1131ded90679930881b2
Robin Nook <ronook@ttu.ee>
2023-10-16 20:37:55 +0300

    good 'ol reset, will try a different approach, added 2 new packages, did some changes to config

--- frontend/src/pages/LoginPage.tsx
+++
@@ -1,5 +0,0 @@
-function LoginPage() {
-    return <h1 className="text 3xl font-bold underline">This is the login page</h1>
-}
-
-export default LoginPage;
--- frontend/src/pages/RegisterPage.tsx
+++
@@ -1,5 +0,0 @@
-function RegisterPage() {
-    return <h1>This is the register page</h1>
-}
-
-export default RegisterPage;
