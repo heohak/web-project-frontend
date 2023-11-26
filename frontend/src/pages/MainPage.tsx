8bb6555d1b33b220fe8ef56d23f6c47d639ebb2b
Robin Nook <robin.nook4@gmail.com>
2023-11-26 21:44:54 +0200

    main view ready-ish

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -3,14 +3,17 @@ import MessagesContainer from '@/components/MessagesContainer';
 import React from 'react';
 
 import '@/styles/scrollbar.css';
+import SwipeContainer from '@/components/SwipeContainer';
 
 
 const MainPage: React.FC = () => {
+    const accountId = '123'; // TODO: Replace with actual accountId fetching logic
+
     return (
         <div className="flex min-h-screen items-center justify-center">
             <MessagesContainer />
-            <div className='bg-fuchsia-500 flex-1'>
-                Swipe area
+            <div className='flex-1 justify-center flex'>
+                <SwipeContainer accountId={accountId} />
             </div>
         </div>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,10 +9,27 @@ export default {
         'primary': { 100: '#fffdf5', 300: '#fff3cc', 500: '#ffefb8'},
         'gray': { 20: '#eeeef1', 50: '#d8d8df', 100: '#b6b7c4', 500: '#72748d'},
         'secondary': { 100: '#ff7366', 300: '#ff2814', 500: '#c21000'},
+        'button-white': { 100: '#e6e7e8'}
       },
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"]
+      },
+      width: {
+        '108': '27rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
+      },
+      height: {
+        '108': '27rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
       },
     }
   },
