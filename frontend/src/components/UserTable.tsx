a48d21985612889ab2e4416d476a64e4ee5f29b8
heohak <heohak@ttu.ee>
2023-12-07 17:41:25 +0200

    added search

--- frontend/src/components/UserTable.tsx
+++ frontend/src/components/UserTable.tsx
@@ -1,5 +1,4 @@
-// UserTable.tsx
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 interface User {
     id: number;
@@ -20,55 +19,76 @@ const UserTable: React.FC = () => {
     const [totalPages, setTotalPages] = useState<number>(0);
     const [sortField, setSortField] = useState<SortField>(null);
     const [sortDirection, setSortDirection] = useState<SortDirection>('none');
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
     useEffect(() => {
-        fetch(`/api/user/paginated?page=${page}&size=${size}`)
-            .then(response => response.json())
-            .then(data => {
-                setUsers(data.content);
-                setTotalPages(data.totalPages);
-            })
-            .catch(error => console.error('Error fetching data: ', error));
-    }, [page, size]);
+        const fetchUsers = async () => {
+            const searchParam = searchTerm ? `&search=${encodeURIComponent(searchTerm)}` : '';
+            const response = await fetch(
+                `/api/user/paginated?page=${page}&size=${size}${searchParam}`
+            );
+            const data = await response.json();
+            const fetchedUsers = data.content;
+
+            if (sortField && sortDirection !== 'none') {
+                fetchedUsers.sort((a: User, b: User) => {
+                    if (a[sortField] < b[sortField]) return sortDirection === 'asc' ? -1 : 1;
+                    if (a[sortField] > b[sortField]) return sortDirection === 'asc' ? 1 : -1;
+                    return 0;
+                });
+            }
+
+            setUsers(fetchedUsers);
+            setTotalPages(data.totalPages);
+        };
+
+        fetchUsers();
+    }, [page, size, sortField, sortDirection, searchTerm]);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target as HTMLInputElement;  // Explicit type assertion
+        setSearchTerm(target.value);
+        setPage(0);
+    };
+
 
     const sortData = (field: SortField) => {
         if (field === sortField) {
-            setSortDirection(prevDirection =>
-                prevDirection === 'asc' ? 'desc' : prevDirection === 'desc' ? 'none' : 'asc'
-            );
+            setSortDirection(prevDirection => prevDirection === 'asc' ? 'desc' : 'asc');
         } else {
             setSortField(field);
             setSortDirection('asc');
         }
     };
 
-    useEffect(() => {
-        if (sortField !== null) {
-            const sortedData = [...users].sort((a, b) => {
-                if (a[sortField] < b[sortField]) return sortDirection === 'asc' ? -1 : sortDirection === 'desc' ? 1 : 0;
-                if (a[sortField] > b[sortField]) return sortDirection === 'asc' ? 1 : sortDirection === 'desc' ? -1 : 0;
-                return 0;
-            });
-            setUsers(sortedData);
-        }
-    }, [sortField, sortDirection]);
 
     const renderSortArrow = (field: keyof User) => {
         return (
             <span className={sortField === field ? 'font-bold' : ''}>
-                {sortDirection === 'asc' ? '↑' : '↓'}
-            </span>
+            {sortField === field && (sortDirection === 'asc' ? '↑' : '↓')}
+        </span>
         );
     };
 
+
     return (
         <div className="container mx-auto p-4 bg-fuchsia-50 rounded-xl shadow-lg">
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="mb-4 p-2 border border-fuchsia-600"
+            />
             <table className="min-w-full leading-normal">
                 <thead>
                 <tr className="text-fuchsia-600 font-bold">
-                    <th className="cursor-pointer" onClick={() => sortData('firstName')}>First Name {renderSortArrow('firstName')}</th>
-                    <th className="cursor-pointer" onClick={() => sortData('lastName')}>Last Name {renderSortArrow('lastName')}</th>
-                    <th className="cursor-pointer" onClick={() => sortData('email')}>Email {renderSortArrow('email')}</th>
+                    <th className="cursor-pointer" onClick={() => sortData('firstName')}>First
+                        Name {renderSortArrow('firstName')}</th>
+                    <th className="cursor-pointer" onClick={() => sortData('lastName')}>Last
+                        Name {renderSortArrow('lastName')}</th>
+                    <th className="cursor-pointer"
+                        onClick={() => sortData('email')}>Email {renderSortArrow('email')}</th>
                     <th>Date of Birth</th>
                     <th>Gender</th>
                 </tr>
@@ -79,14 +99,14 @@ const UserTable: React.FC = () => {
                         <td className="p-2">{user.firstName}</td>
                         <td className="p-2">{user.lastName}</td>
                         <td className="p-2">{user.email}</td>
-                        <td className="p-2">{String(user.dateOfBirth)}</td>
+                        <td className="p-2">{user.dateOfBirth}</td>
                         <td className="p-2">{user.genderMale ? 'Male' : 'Female'}</td>
                     </tr>
                 ))}
                 </tbody>
             </table>
             <div className="pagination flex justify-center mt-4">
-                {Array.from({ length: totalPages }, (_, i) => (
+                {Array.from({length: totalPages}, (_, i) => (
                     <button
                         key={i}
                         onClick={() => setPage(i)}
